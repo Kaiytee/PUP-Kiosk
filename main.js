59ff8b072a9fe04ed9f78aa521ad44ef3a9cc439
Stefen Labinay <150441368+Kaiytee@users.noreply.github.com>
2024-05-14 13:13:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function showSceneryBox(imgSource, heading, description) {
+    let d = document.getElementById("display");
+    let e = document.getElementById("body");
+    e.style.overflow = "hidden";
+    document.addEventListener('mousemove', displayFollowCamera);
+    d.style.left = event.clientX + 5 + "px";
+    d.style.top = event.clientY + 5 + "px";
+    d.innerHTML = "<div id='wrapper' class='box'><img class='imgscene' src='" + imgSource + "'>" + "<h2>" + heading + "<h2>" + "<p>" + description + "</p></div>";
+}
+
+function displayFollowCamera() {
+    let a = document.getElementById("display");
+    a.style.left = event.clientX + 5 + "px";
+    a.style.top = event.clientY + 5 + "px";
+}
+
+function removeSceneryBox() {
+    let a = document.getElementById("display");
+    let b = document.getElementById("body");
+    a.removeAttribute('style');
+    b.removeAttribute('style');
+    document.removeEventListener('mousemove', displayFollowCamera);
+    a.removeChild(a.childNodes[0]);
+}
